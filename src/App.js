3f213d272ab5a12ea7a4912eb674ab03ef6c5441
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-11-07 15:49:24 +0100

    feat(servicesscreen): begin of carousel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header/Header';
 import BannerScreen from './screens/BannerScreen';
 import AboutScreen from './screens/AboutScreen';
+import ServicesScreen from './screens/ServicesScreen';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <BannerScreen className='banner-screen' />
       <AboutScreen />
+      <ServicesScreen />
     </div>
   );
 }
---
+++ src/screens/ServicesScreen.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './styles/servicesScreen.css'
+import BackgroundImage from '../components/background-image/BackgroundImage'
+import background_placeholder from '../assets/images/services_screen_background_placeholder.jpg'
+
+function ServicesScreen() {
+
+    const services=[
+        {id:0, title: "Frontend"},
+        {id:1, title: "GraphicDesigner"},
+        {id:2, title: "Apps"},
+        {id:3, title: "Photographer"},
+        {id:4, title: "Webdesign"},
+        {id:5, title: "Freelancer"},
+        {id:6, title: "VJ"},
+        {id:7, title: "Filmmaker"}
+    ];
+
+  return (
+    <div className='services-screen-container'>
+        <BackgroundImage image={background_placeholder} opacity={0.4} />
+        <div className="services-content-container">
+            <h2 className='services-title'>What I can do for you:</h2>
+            <ul className='services-list'>
+                {services.map((service) => (
+                    <li key={service.id} className='services-list-item'>{service.title}</li>
+                    ))}
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default ServicesScreen
