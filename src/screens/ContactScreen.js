0c8863b648c631ebf31e24735e81d46b081e29f4
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2022-11-30 13:49:11 +0100

    feat(contactScreen): added contact information and start of form

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -10,10 +10,24 @@ function ContactScreen() {
     <div id='contact' className='contact-screen-container'>
         <BackgroundImage image={background_placeholder} opacity={0.7} />
         <div className="contact-content-container">
-        <div className='about-titles'>
-                <h1 className='about-main-title'>Contact</h1>
-                <div className='about-background-title'>Contact</div>
+          <div className='contact-titles'>
+            <h1 className='contact-main-title'>Contact</h1>
+            <div className='contact-background-title'>Contact</div>
+          </div>
+          <h2 className='contact-information-title'>Contact information</h2>
+          <p className='contact-email'>dries.vanmierlo@telenet.be</p>
+          <p className='contact-phone'>+32 470 63 75 28</p>
+          <div className='contact-socials-container'>
+            <a target="_blank" className="contact-social-icon" href="https://www.instagram.com/vanmierlodries/" rel="noreferrer"><span className='icon-instagram'></span></a>
+            <a target="_blank" className="contact-social-icon" href="https://www.facebook.com/dries.vanmierlo.94" rel="noreferrer"><span className='icon-facebook'></span></a>
+            <a target="_blank" className="contact-social-icon" href="https://www.linkedin.com/company/dries-vanmierlo/" rel="noreferrer"><span className='icon-linkedin'></span></a>
+          </div>
+          <form>
+            <div className="contact-form-row">
+              <label htmlFor="">Name</label>
+              <input type="text" />
             </div>
+          </form>
         </div>
         
     </div>
