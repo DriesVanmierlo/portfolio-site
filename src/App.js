675ad9c9e35cf8cf05bc9ab07b44f516201a1795
DriesVanmierlo <dries.vanmierlo@student.ehb.be>
2023-05-26 20:33:54 +0200

    refactor: react router V5

--- src/App.js
+++ src/App.js
@@ -5,34 +5,27 @@ import AboutScreen from "./screens/AboutScreen";
 import ServicesScreen from "./screens/ServicesScreen";
 import PortfolioScreen from "./screens/PortfolioScreen";
 import ContactScreen from "./screens/ContactScreen";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./saveme/screens/Landing";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Switch,
-  useLocation,
-} from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="app-container">
-              <Header />
-              <BannerScreen className="banner-screen" />
-              <AboutScreen />
-              <ServicesScreen />
-              <PortfolioScreen />
-              <ContactScreen />
-            </div>
-          }
-        />
-        <Route path="/saveme" element={<Landing />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <div className="app-container">
+            <Header />
+            <BannerScreen className="banner-screen" />
+            <AboutScreen />
+            <ServicesScreen />
+            <PortfolioScreen />
+            <ContactScreen />
+          </div>
+        </Route>
+        <Route path="/saveme">
+          <Landing />
+        </Route>
+      </Switch>
     </Router>
   );
 }
